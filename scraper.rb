e5d24726dd476e30b70b8aa98194424412ba5109
Tony Bowden <tony@tmtm.com>
2017-01-08 18:29:43 +0000

    rubocop -a

--- scraper.rb
+++ scraper.rb
@@ -1,5 +1,6 @@
 #!/bin/env ruby
 # encoding: utf-8
+# frozen_string_literal: true
 
 require 'scraperwiki'
 require 'nokogiri'
@@ -10,31 +11,29 @@ OpenURI::Cache.cache_path = '.cache'
 # require 'scraped_page_archive/open-uri'
 
 def noko(url)
-  Nokogiri::HTML(open(url).read) 
+  Nokogiri::HTML(open(url).read)
 end
 
-BASE = "http://www.parliament.gov.bw/component/member/"
+BASE = 'http://www.parliament.gov.bw/component/member/'
 url = BASE + '?action=showAll&Itemid=110&limit=500'
 page = noko(url)
 
 data = page.xpath('//tr[.//div[@class="PhotoMemb"]]').map do |mem|
-
   member_page = BASE + mem.xpath('.//span[@class="label" and text()[contains(.,"Name")]]/ancestor::tr[1]/td[2]//a/@href').text
   details = noko(member_page)
   info = {
     # I can't get the sibling-td version of this to work, so up and down again
-    name: details.xpath('.//span[@class="label" and text()[contains(.,"Name")]]/ancestor::tr[1]/td[2]').text.strip.gsub(/\s+/,' '),
-    post: details.xpath('.//span[@class="label" and text()[contains(.,"Designation")]]/ancestor::tr[1]/td[2]').text.strip.gsub(/\s+/,' '),
-    constituency: details.xpath('.//span[@class="labelDetails" and text()[contains(.,"Constituency")]]/ancestor::tr[1]/td[2]').text.strip.gsub(/\s+/,' '),
-    party: details.xpath('.//span[@class="labelDetails" and text()[contains(.,"Party")]]/ancestor::tr[1]/td[2]').text.strip.gsub(/\s+/,' '),
-    email: details.css('a[href^=mailto]').text,
+    name:         details.xpath('.//span[@class="label" and text()[contains(.,"Name")]]/ancestor::tr[1]/td[2]').text.strip.gsub(/\s+/, ' '),
+    post:         details.xpath('.//span[@class="label" and text()[contains(.,"Designation")]]/ancestor::tr[1]/td[2]').text.strip.gsub(/\s+/, ' '),
+    constituency: details.xpath('.//span[@class="labelDetails" and text()[contains(.,"Constituency")]]/ancestor::tr[1]/td[2]').text.strip.gsub(/\s+/, ' '),
+    party:        details.xpath('.//span[@class="labelDetails" and text()[contains(.,"Party")]]/ancestor::tr[1]/td[2]').text.strip.gsub(/\s+/, ' '),
+    email:        details.css('a[href^=mailto]').text,
     # image from the root page
-    image: mem.css('img/@src').text,
-    source: member_page,
+    image:        mem.css('img/@src').text,
+    source:       member_page,
   }
-  info.delete(:image) if info[:image] == "/images/no_photo.jpg"
+  info.delete(:image) if info[:image] == '/images/no_photo.jpg'
   info
 end
 
 ScraperWiki.save_sqlite([:name], data)
-
